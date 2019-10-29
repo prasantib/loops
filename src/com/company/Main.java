e96922cc18d8c4e58ed139f0e31956a4bd2484e3
prasantib <prasanti.babu@gmail.com>
2019-10-29 16:21:53 -0400

    loops

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+/*package com.company;
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++)
+            System.out.println(i);
+    }
+}
+
+ */
+/*package com.company;
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--)
+            System.out.println(i);
+    }
+}
+
+ */
+/*package com.company;
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++)
+            if (i % 2!= 0){
+            System.out.println(i);
+        }
+    }
+}
+
+ */
+/*package com.company;
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++)
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+    }
+}
+
+ */
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++)
+            System.out.println(i + i);
+    }
+}
